cd1ac5a283db927893438647ec7c874265cd1b16
sangress <shai.angress@gmail.com>
2019-11-20 17:06:42 +0200

    feature: prefabs and more scripts

---
+++ GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+
+    public bool isSlowMoMode;
+    public float slowMoScale;
+
+    public bool isPlay() {
+        return false;
+    }
+    public bool isEnd() {
+        return false;
+    }
+}
---
+++ ObjectsPool.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsPool : MonoBehaviour {
+    public static ObjectsPool instance = null;
+
+    public GameObject get(string name, float ttl = 0) {
+		return null;
+	}
+}
