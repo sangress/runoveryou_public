ad4e9715010ed5e1f45e2f847f10ab751e87e683
sangress <shai.angress@gmail.com>
2019-11-20 17:23:06 +0200

    feature: add ToggleRigidbody.cs

---
+++ ToggleRigidbody.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRigidbody : MonoBehaviour {
+	public Rigidbody[] rbs;
+	public Transform hips;
+	[Header("Debug")]
+	public bool isRigidbody;
+
+	private Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+		for (int i = 0; i < rbs.Length; i++) {
+			rbs[i].isKinematic = true;
+			// rbs[i].collisionDetectionMode = CollisionDetectionMode.Continuous;
+		}
+	}
+
+	void FixedUpdate() {
+		if (isRigidbody) {
+			if (anim != null) {
+				anim.enabled = false;
+			}
+			for (int i = 0; i < rbs.Length; i++) {
+				rbs[i].isKinematic = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player" || col.tag == "RoadEnd" || col.tag == "EndGame" || col.tag.Contains("wall")) {
+			if (anim != null) {
+				anim.enabled = false;
+			}
+			for (int i = 0; i < rbs.Length; i++) {
+				rbs[i].collisionDetectionMode = CollisionDetectionMode.Continuous;
+				rbs[i].isKinematic = false;
+			}
+			// TODO: FIXME: reset when using pool!
+			// isRigidbody = true;
+		}
+	}
+}
